67873e58864fd3dd7be8f198bf071f844d3e8927
Chris Gaudette <cgaudette@github.com>
2024-08-30 11:12:55 -0700

    Update profileImageUrlUpload.ts

--- profileImageUrlUpload.ts
+++ profileImageUrlUpload.ts
@@ -16,7 +16,7 @@ module.exports = function profileImageUrlUpload () {
   return (req: Request, res: Response, next: NextFunction) => {
     if (req.body.imageUrl !== undefined) {
       const url = req.body.imageUrl
-      if (url.match(/(.)*solve\/challenges\/server-side(.)*/) !== null) req.app.locals.abused_ssrf_bug = true
+      if ( !== null) req.app.locals.abused_ssrf_bug = true
       const loggedInUser = security.authenticatedUsers.get(req.cookies.token)
       if (loggedInUser) {
         const imageRequest = request
